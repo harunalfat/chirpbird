7dc59694c6b2c727875d0a1996e183ea282af47c
Alfat Saputra Harun <harunalfat@gmail.com>
2022-01-15 10:32:50 +0800

    working HA

--- backend/main.go
+++ backend/main.go
@@ -18,22 +18,6 @@ type App struct {
 	wsHandler   *handlers.WSHandler
 }
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func (app *App) run() {
 	app.wsHandler.Init()
 	router := gin.Default()
